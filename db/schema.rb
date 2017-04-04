2b2154852facc1e3e5ed2823b59bed3f4225db92
Thanh <hhthanh.hust@gmail.com>
2017-04-04 03:53:27 +0000

    CarrierWave

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307040440) do
+ActiveRecord::Schema.define(version: 20170404030835) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20170307040440) do
     t.integer  "number_of_pages"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
